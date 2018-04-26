05f6c39dd7ce998206d4cdcbcf6a7ea5217056f7
Linn <lilo4591@student.uu.se>
2018-04-26 22:44:01 +0200

    renaming testfile

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ app.use(express.static(path.join(__dirname, 'public/')));
 app.use('/vue', express.static(path.join(__dirname, '/node_modules/vue/dist/')));
 
 // Serve test.html directly as root page
-app.get('/test', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views/test.html'));
+app.get('/teacher', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views/teacherpage.html'));
 });
 
-
+/*
 // Serve index.html directly as root page
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'views/teacher.html'));
@@ -45,7 +45,7 @@ app.get('/heteronomy-autonomy1', function (req, res) {
   res.sendFile(path.join(__dirname, 'views/heteronomy-autonomy1.html'));
 });
 
-
+*/
 
 // Serve student.html as /student
 app.get('/student', function (req, res) {
@@ -59,7 +59,6 @@ app.get('/teacher-admin', function (req, res) {
 
 // Store data in an object to keep the global namespace clean and
 // prepare for multiple instances of data if necessary
-
  
 function Data() {
   this.students = [];
@@ -113,63 +112,4 @@ io.on('connection', function(socket) {
   });
 
 });
-/*
-io.of("/test").on('connection', function(socket) {
-  console.log("Teacher with socketID:  " + socket.id + " connected")
-});
-*/
 
-/*
-io.on('connection', function (socket) {
-  // Send list of orders when a client connects
-  socket.emit('initialize', { orders: data.getAllOrders(),
-                              taxis: data.getAllTaxis() });
-  // Add a listener for when a connected client emits an "orderTaxi" message
-  socket.on('orderTaxi', function (order) {
-    var orderId = data.addOrder(order);
-    order.orderId = orderId;
-    // send updated info to all connected clients, note the use of "io" instead of "socket"
-    io.emit('taxiOrdered', order);
-    // send the orderId back to the customer who ordered
-    socket.emit('orderId', orderId);
-  });
-  socket.on('addTaxi', function (taxi) {
-    taxi.driverQueue = 0;
-    data.addTaxi(taxi);
-    // send updated info to all connected clients, note the use of io instead of socket
-    io.emit('taxiAdded', taxi);
-  });
-  socket.on('moveTaxi', function (taxi) {
-    data.updateTaxiDetails(taxi);
-    // send updated info to all connected clients, note the use of io instead of socket
-    io.emit('taxiMoved', taxi);
-  });
-  socket.on('taxiQuit', function (taxi) {
-    data.removeTaxi(taxi);
-    console.log("Taxi",taxi,"has left the job");
-    // send updated info to all connected clients, note the use of io instead of socket
-    io.emit('taxiQuit', taxi);
-  });
-  socket.on('finishOrder', function (orderId) {
-    data.finishOrder(orderId);
-    // send updated info to all connected clients, note the use of io instead of socket
-    io.emit('orderFinished', orderId);
-  });
-
-  socket.on('taxiAssigned', function(order) {
-    data.updateOrderDetails(order);
-    io.emit('currentQueue', { orders: data.getAllOrders() });
-  });
-  socket.on('orderAccepted', function(order) {
-    data.updateOrderDetails(order);
-    data.taxis[order.taxiIdConfirmed].orderQueue.push(order.orderId);
-    io.emit('orderAccepted', order );
-    io.emit('taxiUpdated', data.taxis[order.taxiIdConfirmed] );
-  })
-});
-
-
-var server = http.listen(app.get('port'), function () {
-  console.log('Server listening on port ' + app.get('port'));
-});
-**/
